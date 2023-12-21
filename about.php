3eb5ca785842dea1542de3bce22d77771a185609
l06in <mudin.imam24@gmail.com>
2023-12-21 21:19:53 +0700

    uas

---
+++ about.php
@@ -0,0 +1,26 @@
+<?php require_once('_header.php') ?>
+
+   
+   </div>
+   <div id="about" class="main-content">
+      <div class="container">
+         <div class="baris">
+            <div class="col mt-2">
+               <div class="card">
+                  <div class="card-body">
+                     <div class="card-flex-column">
+                        <div class="about_header">
+                           <img src="<?=url('_assets/img/logo/login.png')?>" alt="Logo rumah laundry" width="220">
+                           <p>Jasa laundry tercepat dan termurah se-priangan timur.</p>
+                        </div>
+                        <div class="about_us">
+                           <h2>- Tentang Kami -</h2>
+                           <p>Rumah Laundry UBSI merupakan suatu pelayanan di bidang jasa yang menggunakan media online sebagai Platform untuk memudahkan admin untuk menginput dan menyimpan data secara lebih cepat, mudah dan aman. Sebagaimana kita ketahui bahwasannya kemudahan mendapatkan berbagai macam jasa pada era sekarang sangatlah diperlukan, selain mudah dan cepat, kami menyediakan berbagai macam paket laundry yang sesuai dengan budget.</p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+<?php require_once('_footer.php') ?>
